4f4bf3316e29da3693e7f8b5856766b5017a3eb0
Marcelina Golinska <marcagol@gmail.com>
2020-07-12 21:42:23 +0200

    code restructured

--- js/script.js
+++ js/script.js
@@ -1,41 +1,47 @@
-{ 
+{
     const welcome = () => {
-        console.log("Przeliczmy parę złotych"); 
+        console.log("Przeliczmy parę złotych");
     }
-    
 
-const formElement = document.querySelector(".js-form");
-const amountElement = document.querySelector(".js-amount");
-const currencyElement = document.querySelector(".js-currency");
-const resultElement = document.querySelector(".js-result");
+    const countResult = (amount, currency) => {
+        const gbpRate = 4.99;
+        const eurRate = 4.46;
+        const czkRate = 0.17;
 
-
-const gbpRate = 4.99;
-const eurRate = 4.46;
-const czkRate = 0.17;
-
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const amount = +amountElement.value;
-    const currency = currencyElement.value;
-
-    let result;
-
-    switch (currency) {
-        case "GBP":
-            result = amount / gbpRate;
-            break;
-        case "EUR":
-            result = amount / eurRate;
-            break;
-        case "CZK":
-            result = amount / czkRate;
-            break;
+        switch (currency) {
+            case "GBP":
+                return amount / gbpRate;
+            case "EUR":
+                return amount / eurRate;
+            case "CZK":
+                return amount / czkRate;
+        }
     };
     
-    resultElement.innerHTML = `Za ${amount.toFixed(2)} PLN kupisz <strong>${result.toFixed(2)}</strong> ${currency}`;   
-    welcome();
-});
+    const showResult = (amount, currency, result) => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerHTML = `Za ${amount.toFixed(2)} PLN kupisz <strong>${result.toFixed(2)}</strong> ${currency}`;
+    };
+
+    const formSubmit = () => {
+        event.preventDefault();
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+        
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
+
+        const result = countResult(amount, currency);
+      
+        welcome();
+        showResult(amount, currency, result);
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", formSubmit);
+    };
+
+    init();
 }
